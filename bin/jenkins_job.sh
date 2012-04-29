b3247f55295d3cdb14c6f5db1d792f053998860c
shirai <tatsuya.py@gmail.com>
2012-04-29 10:08:13 +0900

    delete rake rcov.

--- bin/jenkins_job.sh
+++ bin/jenkins_job.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
-PATH=/usr/local/rvm/bin/:/usr/local/bin:/usr/bin/:/bin:$PATH
-. /etc/profile.d/rvm.sh
+`dirname $0`/rvm.sh
 mkdir -p features/reports
 rvm $1 do bundle install --path=vendor/bundle
 rvm $1 do bundle exec cucumber features -f junit --out features/reports
-rvm $1 do bundle exec rake ci:setup:rspec spec rcov
+rvm $1 do bundle exec rake ci:setup:rspec spec
 
---
+++ bin/rvm.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+PATH=/usr/local/rvm/bin/:/usr/local/bin:/usr/bin/:/bin:$PATH
+. /etc/profile.d/rvm.sh
